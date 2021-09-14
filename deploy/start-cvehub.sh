75065de55c0d574e38db7c4590633b9ec5d2938e
sunhanwu <sunhanwu@sunhanwus-Mac-mini.local>
2021-09-15 00:21:39 +0800

    change deploy script

--- deploy/start-cvehub.sh
+++ deploy/start-cvehub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /home/sunhanwu/cvehub/
-chmod +x cvehub-0.0.1-SNAPSHOT.jar
-setsid nohup java -jar cvehub-0.0.1-SNAPSHOT.jar &
+chmod +x ./target/cvehub-0.0.1-SNAPSHOT.jar
+setsid nohup java -jar ./target/cvehub-0.0.1-SNAPSHOT.jar &
