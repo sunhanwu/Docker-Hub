d42da32278de3e7240b4baaa952d25ddcf05cc86
sunhanwu <sunhanwu@sunhanwus-Mac-mini.local>
2021-09-23 22:02:32 +0800

    fix docker deploy script

--- deploy/start-cvehub-docker.sh
+++ deploy/start-cvehub-docker.sh
@@ -2,6 +2,6 @@
 
 echo "Ydm186919" | sudo -S docker stop cvehub
 sudo docker rm cvehub
-sudo docker build -t cvehub:v1
+sudo docker build -t cvehub:v1 .
 sudo docker run --name cvehub -p 8080:8080 cvehub:v1
 
